f468b381d6040f5757a0028c21d7d766c51616b4
heisluft <heisluftlp@gmail.com>
2022-02-11 12:58:08 +0100

    Adapt RGSMappingsProvider to the new Mappings System

--- src/main/java/de/heisluft/reveng/mappings/RGSMappingsProvider.java
+++ src/main/java/de/heisluft/reveng/mappings/RGSMappingsProvider.java
@@ -8,32 +8,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
+/**
+ * RGS Files are mappings for RetroGuard.
+ * They have way more options than just mappings, providing accessModifiers and such things.
+ * 4 Directives are relevant to us: .class_map, .field_map, .method_map and .class.
+ * While the first three are self-explanatory
+ * the .class allows package relocations and access modifiers.
+ * We only parse in the former.
+ *
+ */
+//TODO: Support dynamic package relocation by subclassing Mappings
 public class RGSMappingsProvider implements MappingsProvider, Util {
 
-  public static final RGSMappingsProvider INSTANCE = new RGSMappingsProvider();
+  /**
+   * A singleton instance is used for parsing mappings.
+   */
+  static final RGSMappingsProvider INSTANCE = new RGSMappingsProvider();
 
-  private void convert() throws Exception {
-    List<String> lines = Files.readAllLines(Paths.get("test-rgs/minecraft.rgs"));
-    List<String> out = new ArrayList<>();
-    lines.stream().filter(line -> line.startsWith(".class_map")).map(s -> s.replace(".class_map", "CL:"))
-        .sorted(String::compareTo).forEach(out::add);
-    lines.stream().filter(line -> line.startsWith(".field_map")).map(s -> s.substring(11))
-        .map(s -> s.split(" ")).map(Tuple2::from)
-        .map(t -> t.map1(s -> splitAt(s, s.lastIndexOf('/'))).map1(Tuple2::from))
-        .filter(t -> !t._2.equals(t._1._2)).map(t -> t.map1(join("FD: ", " ")))
-        .map(join("", " ")).sorted(String::compareTo).forEach(out::add);
-    lines.stream().filter(line -> line.startsWith(".method_map")).map(s -> s.substring(12))
-        .map(s -> s.split(" ")).map(args -> {
-          String[] split = splitAt(args[0], args[0].lastIndexOf('/'));
-          if(split[1].equals(args[2])) return "";
-          return "MD: " + split[0] + " " + split[1] + " " + args[1] + " " + args[2];
-        }).filter(s -> !s.isEmpty()).sorted(String::compareTo).forEach(out::add);
-    Files.write(Paths.get("a1.1.2_01.frg"), out);
-  }
-
+  /**
+   * A function to join a string tuple, prepending a prefix and inserting a delimiter in between.
+   * @param prefix the prefix to prepend
+   * @param delim the delimiter to insert
+   * @return the joined String
+   */
   private Function<Tuple2<String, String>,String> join(String prefix, String delim) {
     return tuple -> prefix + tuple._1 + delim + tuple._2;
   }
@@ -41,8 +43,44 @@ public class RGSMappingsProvider implements MappingsProvider, Util {
   @Override
   public Mappings parseMappings(Path path) throws IOException {
     List<String> lines = Files.readAllLines(path);
+    Map<String, String> classNames = new HashMap<>();
+
     Mappings mappings = new Mappings();
-    //TODO: Find a way to conveniently generate
+    List<String> globs = new ArrayList<>();
+    for(String line : lines) {
+      if(line.startsWith("#") || line.isEmpty()) continue;
+      String[] words = line.split(" ");
+      if(words.length < 2)
+        throw new IllegalArgumentException("Directive given with no arguments! (line '" + line + "')");
+      switch(words[0]) {
+        case ".class":
+          if(words.length > 2) break;
+          globs.add(words[1]);
+          break;
+        case ".class_map":
+          if(words.length < 3)
+            throw new IllegalArgumentException("Error on line '" + line + "'. Expected at least 2 arguments, got" + (words.length - 1));
+          classNames.put(words[1], words[2]);
+          break;
+        case ".field_map":
+          if(words.length < 3)
+            throw new IllegalArgumentException("Error on line '" + line + "'. Expected at least 2 arguments, got" + (words.length - 1));
+          String[] fd = splitAt(words[1], words[1].lastIndexOf('/'));
+          getOrPut(mappings.fields, fd[0], new HashMap<>()).put(fd[1], words[2]);
+          break;
+        case ".method_map":
+          if(words.length < 4)
+            throw new IllegalArgumentException("Error on line '" + line + "'. Expected at least 3 arguments, got" + (words.length - 1));
+          String[] md = splitAt(words[1], words[1].lastIndexOf('/'));
+          getOrPut(mappings.methods, md[0], new HashMap<>()).put(new Tuple2<>(md[1], words[2]), words[3]);
+          break;
+      }
+    }
+    mappings.classes.putAll(classNames); // We would need a subclass for RGSMappings if we were to support package relocation.
     return mappings;
+  }
+
+  public static void main(String[] args) throws IOException{
+    RGSMappingsProvider.INSTANCE.parseMappings(Paths.get(args[0]));
   }
 }
