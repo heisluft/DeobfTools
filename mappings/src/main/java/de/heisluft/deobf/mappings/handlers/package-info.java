4530032993da760865ce1d90a9cb9411f75b921c
heisluft <heisluftlp@gmail.com>
2025-01-17 15:51:48 +0100

    Delete now independent files

--- mappings/src/main/java/de/heisluft/deobf/mappings/handlers/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains all default implementations of MappingHandlers.
- */
-package de.heisluft.deobf.mappings.handlers;
--- mappings/src/main/java/de/heisluft/deobf/mappings/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains the library able to build, read, write and query mappings.
- */
-package de.heisluft.deobf.mappings;
